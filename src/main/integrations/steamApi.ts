4a077441810ccfe6c8f0702360273b63770d6533
Evan <lintonevan@gmail.com>
2024-10-30 15:34:33 -0300

    Added: caching for game info and trending games

--- src/main/integrations/steamApi.ts
+++ src/main/integrations/steamApi.ts
@@ -1,6 +1,8 @@
 import { Game, SteamAppDetails } from '@types'
 import * as cheerio from 'cheerio'
 
+const ONE_HOUR = 60 * 60 * 1000
+
 export default async function steamSearch(query: string): Promise<Game[]> {
   const url = `https://store.steampowered.com/search/?term=${encodeURIComponent(query)}&supportedlang=english&category1=998`
 
@@ -37,7 +39,15 @@ export default async function steamSearch(query: string): Promise<Game[]> {
   }
 }
 
+const gameInfoCache: { [key: number]: { data: SteamAppDetails | null; timestamp: number } } = {}
+
 export async function getSteamGameInfo(appId: number): Promise<SteamAppDetails | null> {
+  const currentTime = Date.now()
+
+  if (gameInfoCache[appId] && currentTime - gameInfoCache[appId].timestamp < ONE_HOUR) {
+    return gameInfoCache[appId].data
+  }
+
   const url = `https://store.steampowered.com/api/appdetails?appids=${appId}`
 
   try {
@@ -50,6 +60,11 @@ export async function getSteamGameInfo(appId: number): Promise<SteamAppDetails |
     const data = await response.json()
     const format = data[`${appId}`].data
 
+    gameInfoCache[appId] = {
+      data: format ? (format as SteamAppDetails) : null,
+      timestamp: currentTime
+    }
+
     return format ? (format as SteamAppDetails) : null
   } catch (error) {
     console.error('Error fetching data from Steam:', error)
@@ -57,7 +72,16 @@ export async function getSteamGameInfo(appId: number): Promise<SteamAppDetails |
   }
 }
 
+let trendingCache: Game[] | null = null
+let trendingCacheTimestamp: number | null = null
+
 export async function getTrendingGames(): Promise<Game[]> {
+  const currentTime = Date.now()
+
+  if (trendingCache && trendingCacheTimestamp && currentTime - trendingCacheTimestamp < ONE_HOUR) {
+    return trendingCache
+  }
+
   const url = `https://store.steampowered.com/search/?category1=998&os=win&supportedlang=english&hidef2p=1&filter=globaltopsellers&ndl=1`
 
   try {
@@ -84,6 +108,8 @@ export async function getTrendingGames(): Promise<Game[]> {
       })
     })
 
+    trendingCache = results
+    trendingCacheTimestamp = currentTime
     return results
   } catch (error) {
     console.error('Error fetching data from Steam:', error)
